a216378429858f82d24164b4813ebaf7e7113e1b
ajhanson22227 <ajhanson22227@gmail.com>
2020-10-13 20:44:24 -0500

    Switch Units with button click

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const config = require('./config.js');
 const apiKey = config.weatherAPIKey;
 
 const App = () => {
     const [cityName, setCityName]       = useState('Fargo');
-    const [countyName, setCountryName]  = useState('us');
+    const [countryName, setCountryName]  = useState('us');
     const [tempUnit, setTempUnit]       = useState('imperial');
     const [temp, setTemp]               = useState('');
 
     async function getWeather(){
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName},${countyName}&units=${tempUnit}&APPID=${apiKey}`, {mode: 'cors'});
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryName}&units=${tempUnit}&APPID=${apiKey}`, {mode: 'cors'});
         const weatherData = await response.json();
         console.log(weatherData);
         setTemp(Math.round(weatherData.main.temp))
     }
-    getWeather();
 
+    useEffect( () => {
+        getWeather();
+    }, [cityName, countryName, tempUnit])
+    
     const handleClick = () => {
         tempUnit === 'imperial' ? setTempUnit('metric') : setTempUnit('imperial');
-        getWeather();
     }
 
     return (
         <div>
-            <div>{cityName} is {} {countyName} and boy is it {temp} {tempUnit === 'imperial' ? 'F' : 'C'}</div>
+            <div>{cityName} is {} {countryName} and boy is it {temp} {tempUnit === 'imperial' ? 'F' : 'C'}</div>
             <button onClick={handleClick}>Unit</button>
         </div>
     );
