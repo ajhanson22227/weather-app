76e948d92994a491dbef3ab2af9f01d743c905f3
ajhanson22227 <ajhanson22227@gmail.com>
2020-10-13 20:35:30 -0500

    Call API and return information

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const config = require('./config.js');
+const apiKey = config.weatherAPIKey;
+
+const App = () => {
+    const [cityName, setCityName]       = useState('Fargo');
+    const [countyName, setCountryName]  = useState('us');
+    const [tempUnit, setTempUnit]       = useState('imperial');
+    const [temp, setTemp]               = useState('');
+
+    async function getWeather(){
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName},${countyName}&units=${tempUnit}&APPID=${apiKey}`, {mode: 'cors'});
+        const weatherData = await response.json();
+        console.log(weatherData);
+        setTemp(Math.round(weatherData.main.temp))
+    }
+    getWeather();
+
+    const handleClick = () => {
+        tempUnit === 'imperial' ? setTempUnit('metric') : setTempUnit('imperial');
+        getWeather();
+    }
+
+    return (
+        <div>
+            <div>{cityName} is {} {countyName} and boy is it {temp} {tempUnit === 'imperial' ? 'F' : 'C'}</div>
+            <button onClick={handleClick}>Unit</button>
+        </div>
+    );
 }
 
 export default App;
